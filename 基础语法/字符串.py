9b2106a1855266364090caa68e4eec0a3865a6bd
yanglilin <2686333594@qq.com>
2023-03-14 14:14:34 +0800

    字符串操作

--- 基础语法/字符串.py
+++ 基础语法/字符串.py
@@ -10,3 +10,19 @@ name3 = """third"""#支持换行输入字符串
 2.转移字符 \
 """
 print("\"test") 
+
+#print可以拼接字符串，数字不能
+print(name1 + name2)
+
+#在字符串里面拼接,多个则用括号
+message = "顺序是：%s,%s" % (name1,name2)
+print(message)
+
+#字符串格式化 f"{占位}
+#适合对精度要求不高
+num = 2006
+print(f"我是{name2},今年{num}")
+
+name = input("请输入你的名字：")
+print(f"name:{name}")
+
